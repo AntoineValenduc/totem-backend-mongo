f00e09435abd95c220701e79f69b82e69ae4737d
AntoineValenduc <ant.valenduc@gmail.com>
2025-05-07 11:23:45 +0200

    Docker installation

--- apps/totem-mongo/src/totem-mongo.module.ts
+++ apps/totem-mongo/src/totem-mongo.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ProfileController } from './controllers/profile.controller';
 import { ProfileService } from './services/profile.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { Profile, ProfileSchema } from './schema/profile.schema';
 import { Branch, BranchSchema } from './schema/branch.schema';
 import { Badge, BadgeSchema } from './schema/badge.schema';
@@ -9,10 +8,21 @@ import { BranchController } from './controllers/branch.controller';
 import { BranchService } from './services/branch.service';
 import { BadgeController } from './controllers/badge.controller';
 import { BadgeService } from './services/badge.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/totemDB'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI ?? 'mongodb://127.0.0.1:27017/totemDB',
+    ),
     MongooseModule.forFeature([
       { name: Profile.name, schema: ProfileSchema },
       { name: Branch.name, schema: BranchSchema },
