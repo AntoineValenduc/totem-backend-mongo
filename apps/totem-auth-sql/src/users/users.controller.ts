1084b5526a269fc8d95e02c833040baead149878
AntoineValenduc <ant.valenduc@gmail.com>
2025-06-23 17:25:33 +0200

    fix eslint

--- apps/totem-auth-sql/src/users/users.controller.ts
+++ apps/totem-auth-sql/src/users/users.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-redundant-type-constituents */
 import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -7,7 +8,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body() dto: Promise<CreateUserDto>) {
-    return this.usersService.create(await dto);
+  async create(@Body() dto: CreateUserDto) {
+    return this.usersService.create(dto);
   }
 }
--- apps/totem-auth-sql/src/users/users.service.ts
+++ apps/totem-auth-sql/src/users/users.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-redundant-type-constituents */
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -9,7 +10,7 @@ export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
   async findByEmail(email: string): Promise<User | null> {
-    return this.prisma.user.findUnique({ where: { email } });
+    return await this.prisma.user.findUnique({ where: { email } });
   }
 
   async create(data: CreateUserDto) {
